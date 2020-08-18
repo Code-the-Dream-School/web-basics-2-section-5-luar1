9973e5d898bd18b4430b641a7e1813f1f8e0b531
Lu Ar <magpe.arambula@gmail.com>
2020-08-18 01:26:37 -0500

    Updated Assignment

--- index.js
+++ index.js
@@ -1,38 +1,154 @@
+const getPlayers = () => {
+  let parentPlayer1 = document.querySelector('#name_player1');
+  let divChildOne = document.createElement('div');
+  divChildOne.innerHTML =`${player1.name}`;
+  parentPlayer1.appendChild(divChildOne);
 
-//------------------------ Game Project---------------------------
-//Do you remember the game Battleship we created before? well .... it is time to make it with the DOM!!
-//We are providing you with the design of a board (in the DOM) for a player1, you have to create the board for the player2 using the id property 'board_player2' -> it is the second list (ul) in your index.html file
-//First ask the players for their names (use propmt)
-//Now each time the turn player clicks on any cell of the opponent's board (you have to verify if the player is clicking the right board) the program needs to verify if there is an opponent's ship in that cell. If it is then the opponent has one less ship
-//We want you to store the data of each player in two Player objects. Each object has to store: name, remaining boats, and their respective board.
-//Each board needs to be initialized randomly with '0' and four '1' wich means the state of the cell. Numbers 1 are representing the 4 positions of the player's ships
-//Also we want you to display the name of the turn player in the tag that has the id 'turn_player'. And if there is a winner  a text with: 'Congratulationes {name_player}!! you win'
-//in the index.html file you are going to find 4 more ids: 'name_player1' , 'name_player2' , 'ships_player1' , 'ships_player2'. We want to see the information of each player in the respective elements
-//As our previous Battleship, the winner is the player that hits the 4 opponent's ships first
-//one more Thing create a 'reset' and a 'new game' buttons as childs of the element with the id 'buttons'. the reset button has to start the game again and the new game create a new game with new players and a new random board.
-
-const board_Player1 = document.getElementById('board_player1');
-
-for (var x = 0; x < 4; x++) {
-
-    const li = document.createElement('li'); // creating childs for the list (board), in this case represent a row number 'x' of the board
-
-    for (var y = 0; y < 4; y++) {
-      const cell = document.createElement('div');
-      cell.className = "square"; // adding css properties to make it looks like a square
-      cell.textContent = `${x},${y}`;  // saves the coordinates as a string value 'x,y'
-      cell.value = 0;//state of the cell
-
-      //this function adds the click event to each cell
-      cell.addEventListener( 'click', (e) => {
-          let cell = e.target; // get the element clicked
-          console.log( cell.textContent) //display the coordinates in the console
-          cell.style.visibility = 'hidden';// this  means that the contents of the element will be invisible, but the element stays in its original position and size / try it clicking on any of the black cells (in your browser) and see whats happens
-          //cell.style.background ="purple"; //with this propertie you can change the background color of the clicked cell. try comment the line bellow and uncomment this line. Do not forget to save this file and refresh the borwser to see the changes
-      });
-
-      li.appendChild(cell); //adding each cell into the row number x
-    }
-
-     board_Player1.appendChild(li); //adding each row into the board
+  let parentPlayer2 = document.querySelector('#name_player2');
+  let divChildTwo = document.createElement('div');
+  divChildTwo.innerHTML =`${player2.name}`;
+  parentPlayer2.appendChild(divChildTwo);
 }
+
+
+const board_Player1 = document.querySelector('#board_player1');
+const board_Player2 = document.querySelector('#board_player2');
+const player1_lives = document.querySelector('#ships_player1');
+const player2_lives = document.querySelector('#ships_player2');
+const playerTurn = document.querySelector('#turn_player');
+
+
+const buttons = document.querySelector('#buttons');
+const resetButton = document.createElement('button');
+const newButton = document.createElement('button');
+buttons.appendChild(resetButton);
+buttons.appendChild(newButton);
+resetButton.textContent = 'Reset';
+newButton.textContent = 'New Game';
+
+
+
+const player1 = {
+  name: prompt('Name of Player 1'), 
+  shipsLeft: player1_lives.innerHTML = 4
+
+  }
+
+const player2 = {
+  name: prompt('Name of Player 2'),
+  shipsLeft: player2_lives.innerHTML = 4
+};
+
+
+
+function createIcon(cellBkg) {
+  cellBkg.style.backgroundImage = 'url("ship.png")';
+}
+
+
+const addShips = board => {
+  for(let ship = 0; ship < 4; ship++) {
+    let xCoord = Math.floor(Math.random() * 4);
+    let yCoord = Math.floor(Math.random() * 4);
+    let cell1 = board.getElementsByTagName('li')[xCoord];
+    let cell2 = cell1.getElementsByTagName('div')[yCoord];
+      if (cell2.value === 0) {
+        cell2.value = 1;
+      }else{continue}
+  }
+}
+
+
+const createPlayersBoard = cells => {
+    for (var x = 0; x < 4; x++) {
+      const li = document.createElement('li'); 
+        for (var y = 0; y < 4; y++) {
+          const cell = document.createElement('div');
+          cell.className = "square"; 
+          cell.textContent = ''; 
+          cell.value = 0;
+          cell.addEventListener( 'click', battleShip);
+          li.appendChild(cell); 
+        }
+       cells.appendChild(li); 
+    }
+  }
+
+  
+
+
+const removeMsg = () => {
+  let oldMessage_Winner = document.querySelector('.turn-winner p');
+  oldMessage_Winner.style.display = "none";
+  
+  }
+
+const buttonsActions = (e) => {
+  let button = e.target;
+  if (button === newButton) {
+    location.reload();
+  } else if (button === resetButton) {
+    player1_lives.innerText= 4;
+    player2_lives.innerText = 4;
+    board_Player1.innerText = ''; 
+    board_Player2.innerText = '';
+    removeMsg();
+    createPlayersBoard(board_Player1);
+    addShips(board_Player1);
+    createPlayersBoard(board_Player2);
+    addShips(board_Player2);
+  }
+}
+
+let currentPlayer = player1;
+playerTurn.textContent = player1.name;
+const message = document.querySelector('.turn-winner');
+let message_Winner = document.createElement('p');
+
+
+const battleShip = (e) => {
+  let cell = e.target; 
+  message_Winner.innerHTML = '';
+  message.appendChild(message_Winner);
+  if (currentPlayer === player1 && board_Player2.contains(cell)) {
+    if (cell.value === 1) {
+      createIcon(cell);
+      player2_lives.textContent--;
+      currentPlayer = player1;
+      if (player2_lives.textContent === "0") {
+        message_Winner.style.display = "block";
+        message_Winner.innerHTML =`${currentPlayer.name}. WINS!`;
+      }
+    } else if (cell.value === 0) {
+      cell.style.backgroundImage = 'url("explosion.png")';
+      currentPlayer = player2;
+      playerTurn.textContent = player2.name;
+    } 
+  } else if (currentPlayer === player2 && board_Player1.contains(cell)) {
+    if (cell.value === 1) {
+      createIcon(cell);
+      player1_lives.textContent--;
+      currentPlayer = player2;
+      if (player1_lives.textContent === "0") {
+        message_Winner.style.display = "block";
+        message_Winner.innerHTML =`${currentPlayer.name}. WINS!`;
+      }
+    } else if (cell.value === 0) {
+      currentPlayer = player1;
+      cell.style.backgroundImage = 'url("explosion.png")';
+      playerTurn.textContent = player1.name;
+    }
+  }
+  return `${currentPlayer.name} WINS!`;
+}
+
+
+resetButton.addEventListener('click', buttonsActions);
+newButton.addEventListener('click', buttonsActions);
+
+getPlayers ();
+createPlayersBoard(board_Player1);
+addShips(board_Player1);
+createPlayersBoard(board_Player2);
+addShips(board_Player2);
+  
